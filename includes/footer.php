4636a1aa1b7d8c3980bd8df2ade1744f5ca1addf
Thibeylard <thibaut.beylard@lilo.org>
2020-04-12 19:46:37 +0200

    removed php files

--- includes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-  <a href="https://www.facebook.com/bro7hers/"><img src="img/fb_icon.png" alt="Facebook"/></a>
-  <a href="https://twitter.com/Bro7hersTeam"><img src="img/twitter_icon.png" alt="Twitter"/></a>
-  <a href="https://bro7hers.itch.io/"><img src="img/itch_icon.png" alt="Itch.io"/></a>
-</footer>
--- includes/header.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $filepath = $_SERVER["PHP_SELF"];
-    $fileName = explode("/",$filepath)[2];
-?>
-
-<header>
-  <img src="img/logo.png" alt="Logo Bro7hers">
-</header>
