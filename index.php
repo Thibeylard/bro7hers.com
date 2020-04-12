4ab13bed5d75f052b95ac2f5d0dcde253866179e
Thibeylard <thibaut.beylard@lilo.org>
2020-04-12 12:35:59 +0200

    refactored text css

--- index.php
+++ index.php
@@ -10,22 +10,22 @@
     <nav id="languageNavbar"></nav>
     <section id="introSection">
         <h2 class="sectionTitle">Qui sommes-nous ?</h2>
-        <p class="plainText">Nous sommes une fratrie. <br>
+        <p class="kapsText">Nous sommes une fratrie. <br>
             Depuis notre enfance, nous partageons sans doute comme vous une passion
             pour le jeu vidéo. Tout a commencé avec RPG Maker 95 qui ne marchait pas vraiment.
             Puis est venu l'âge d'or RPG Maker XP. C'est à partir de là que les choses sérieuses ont
             commencé. Depuis, notre parcours a été semé d'embuches, d'abandons, de souffles nouveaux.</p>
-        <p class="plainText">Mais le désir brûlant de créer un univers, de partager avec d'autres nos univers, ne nous a
+        <p class="kapsText">Mais le désir brûlant de créer un univers, de partager avec d'autres nos univers, ne nous a
             jamais quitté. C'est pour ça qu'on est là, qu'on essaye.</p>
-        <p class="plainText">On espère que vous serez avec nous sur la suite de cette aventure.</p>
-        <p class="plainText">Si oui, alors bienvenue, et merci !!</p>
+        <p class="kapsText">On espère que vous serez avec nous sur la suite de cette aventure.</p>
+        <p class="kapsText">Si oui, alors bienvenue, et merci !!</p>
     </section>
     <section id="gameSection">
         <h2 class="pixelTitle">SELECT THE LEVEL</h2>
         <div id="gameScroll">
             <div class="gameWrapper">
                 <img class="gamePicture" src="img/gobBanner.png">
-                <p class="gameDescription plainText">
+                <p class="gameDescription kapsText">
                     Game of BOX a été créé pour le concours Game à Niaque 2015 sur le thème de la "boite".
                     Entre quiz et mini-jeux, essayez de battre notre record de 3500 points, et postez vos screens
                     sur notre page Facebook !
@@ -33,7 +33,7 @@
             </div>
             <div class="gameWrapper">
                 <img class="gamePicture" src="img/modBanner.png">
-                <p class="gameDescription plainText">
+                <p class="gameDescription kapsText">
                     Notre participation au "Underground Jam" sur le site Scirra (Construct 2 et 3) en 2015 !
                     Le jeu ? Incarnez un Commando de Taupe dans un jeu de puzzle et de rapidité pour défaire votre pire ennemi,
                     Mr Mecha, le fermier sans scrupules.
