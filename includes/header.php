6d86e139c862ec437c8ed24c0687e920718dee50
Thibeylard <thibaut.beylard@lilo.org>
2020-04-12 11:21:33 +0200

    css reset + add fonts

--- includes/header.php
+++ includes/header.php
@@ -5,28 +5,4 @@
 
 <header>
   <img src="img/logo.png" alt="Logo Bro7hers">
-  <nav>
-    <table>
-      <tr>
-        <td></td>
-        <td class="menuBox"
-          <?php if($fileName == 'index.php')
-                  echo "id=\"highlightedMenuBox\"";?>
-          ><a href="index.php">Tweets</a></td>
-        <td class="menuBox"
-          <?php if($fileName == 'moleOfDuty.php')
-                  echo "id=\"highlightedMenuBox\"";?>
-          ><a href="moleOfDuty.php">Mole Of Duty</a></td>
-        <td class="menuBox"
-          <?php if($fileName == 'gameOfBox.php')
-                  echo "id=\"highlightedMenuBox\"";?>
-          ><a href="gameOfBox.php">Game of Box</a></td>
-        <td class="menuBox"
-          <?php if($fileName == 'team.php')
-                  echo "id=\"highlightedMenuBox\"";?>
-          ><a href="team.php">L'équipe</a></td>
-        <td></td>
-      </tr>
-    </table>
-  </nav>
 </header>
--- index.php
+++ index.php
@@ -10,22 +10,22 @@
     <nav id="languageNavbar"></nav>
     <section id="introSection">
         <h2 class="sectionTitle">Qui sommes-nous ?</h2>
-        <p>Nous sommes une fratrie. <br>
+        <p class="plainText">Nous sommes une fratrie. <br>
             Depuis notre enfance, nous partageons sans doute comme vous une passion
             pour le jeu vidéo. Tout a commencé avec RPG Maker 95 qui ne marchait pas vraiment.
             Puis est venu l'âge d'or RPG Maker XP. C'est à partir de là que les choses sérieuses ont
             commencé. Depuis, notre parcours a été semé d'embuches, d'abandons, de souffles nouveaux.</p>
-        <p>Mais le désir brûlant de créer un univers, de partager avec d'autres nos univers, ne nous a
+        <p class="plainText">Mais le désir brûlant de créer un univers, de partager avec d'autres nos univers, ne nous a
             jamais quitté. C'est pour ça qu'on est là, qu'on essaye.</p>
-        <p>On espère que vous serez avec nous sur la suite de cette aventure.</p>
-        <p>Si oui, alors bienvenue, et merci !!</p>
+        <p class="plainText">On espère que vous serez avec nous sur la suite de cette aventure.</p>
+        <p class="plainText">Si oui, alors bienvenue, et merci !!</p>
     </section>
     <section id="gameSection">
         <h2 class="pixelTitle">SELECT THE LEVEL</h2>
         <div>
             <div class="gameWrapper">
                 <img class="gamePicture">
-                <p class="gameDescription"></p>
+                <p class="gameDescription plainText"></p>
             </div>
         </div>
         <nav id="gameNavbar"></nav>
@@ -38,7 +38,7 @@
             <img class="broPicture" id="thibPicture"/>
         </nav>
         <h3 class="smallTitle"></h3>
-        <p id="broDescription"></p>
+        <p id="broDescription" class="plainText"></p>
     </section>
     <?php include("includes/footer.php");?>
   </body>
