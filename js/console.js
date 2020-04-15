7e2439dad92be577c3aafa5c5d0f3c2a99be504a
Thibeylard <thibaut.beylard@lilo.org>
2020-04-15 18:45:39 +0200

    No value for bro or game in console.html : redirection to index.html

--- js/console.js
+++ js/console.js
@@ -9,16 +9,14 @@ function setBro(broName) {
     if(broName === "kent") {
         $("#selectedBroName").html("Kentyn")
         $("#selectedBroPicture").attr("src","img/broKent.png")
-    }
-
-    if(broName === "gaut") {
+    } else if (broName === "gaut") {
         $("#selectedBroName").html("Gautier")
         $("#selectedBroPicture").attr("src","img/broGaut.png")
-    }
-
-    if(broName === "thib") {
+    } else if(broName === "thib") {
         $("#selectedBroName").html("Thibaut")
         $("#selectedBroPicture").attr("src","img/broThib.png")
+    } else {
+        window.location.href = "index.html";
     }
 }
 
@@ -26,11 +24,11 @@ function setGame (gameName) {
     if(gameName === "gob") {
         $("#consoleSection").html("<iframe src=\"https://itch.io/embed-upload/117126?color=f28112\" allowfullscreen=\"\" width=\"640\" height=\"500\" frameborder=\"0\"><a href=\"https://bro7hers.itch.io/game-of-box\">Play GAME OF BOX on itch.io</a></iframe>")
         $("#gameCartridge img").attr("src","img/gobCartridge.png")
-    }
-
-    if(gameName === "mod") {
+    } else if(gameName === "mod") {
         $("#consoleSection").html("<iframe src=\"https://itch.io/embed-upload/1796782?color=0b450b\" allowfullscreen=\"\" width=\"640\" height=\"500\" frameborder=\"0\"><a href=\"https://bro7hers.itch.io/mole-of-duty\">Play MOLE OF DUTY on itch.io</a></iframe>")
         $("#gameCartridge img").attr("src","img/modCartridge.png")
+    } else {
+        window.location.href = "index.html";
     }
 }
 
